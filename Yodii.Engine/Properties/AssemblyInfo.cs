51ad20b010cef4039d9f2e1b6677a1b980e2d9f0
Olivier Spinelli <olivier.spinelli@invenietis.com>
2013-03-17 13:37:56 +0100

    Project initialization.
    - Classical Model/Runtime/Tests structure.
    - No code at all nor dependency to CK.Core yet.
    - LGPL licence initialized on all Yodii projects.

---
+++ Yodii.Engine/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿#region LGPL License
+/*----------------------------------------------------------------------------
+* This file (Yodii.Engine\Properties\AssemblyInfo.cs) is part of Yodii project. 
+*  
+* Yodii is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU Lesser General Public License as published 
+* by the Free Software Foundation, either version 3 of the License, or 
+* (at your option) any later version. 
+*  
+* Yodii is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+* GNU Lesser General Public License for more details. 
+* You should have received a copy of the GNU Lesser General Public License 
+* along with Yodii.  If not, see <http://www.gnu.org/licenses/>. 
+*  
+* Copyright © 2013, Invenietis <http://www.invenietis.com>
+* All rights reserved. 
+*-----------------------------------------------------------------------------*/
+#endregion
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle( "Yodii.Engine" )]
+[assembly: AssemblyDescription( "Yodii engine." )]
+[assembly: AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "2EB4747E-2B1F-4329-BC97-FC03E8B97F62" )]
+
+// Allow Yodii.Engine.Tests assembly to acces to internals of Yodii.Engine (allow white testing of Yodii.Engine).
+[assembly: InternalsVisibleTo( "Yodii.Engine.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001002badda7c6774254194bd7d7b264aa7be4622e8a0105acfe1b2edc239b3389a317e008862dd5c62b61298042874b8bf08c4ad18a71dcbae5234066d3f6ef159bc9f8014c89d5be68f4d5b59af4169f15784af3eb2fa02e312e480ea123f383c09bab56a016b46519cc830fa17bd6ccff7260cc8d20ece42745cef70b98e3c70d9" )]
