5238ca2b0aa6e8959cf5834616a9d7cf5987d3ab
aztheroth <bontemps@intechinfo.fr>
2013-11-20 10:31:47 +0100

    create EmptyDiscoveredInfo

---
+++ Yodii.Engine/EmptyDiscoveredInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Yodii.Model;
+using CK.Core;
+
+namespace Yodii.Engine
+{
+    public class EmptyDiscoveredInfo : IDiscoveredInfo
+    {
+        public IReadOnlyList<IServiceInfo> ServiceInfos
+        {
+            get { return CKReadOnlyListEmpty<IServiceInfo>.Empty; }
+        }
+
+        public IReadOnlyList<IPluginInfo> PluginInfos
+        {
+            get { return CKReadOnlyListEmpty<IPluginInfo>.Empty; }
+        }
+    }
+}
