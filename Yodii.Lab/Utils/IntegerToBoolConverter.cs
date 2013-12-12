a43ff5bd86ce7cfdb339b873b3f9b844ef3e4fe2
Benjamin Crosnier <bcrosnier@intechinfo.fr>
2013-12-12 11:13:26 +0100

    Various UI and ergo enhancements.

---
+++ Yodii.Lab/Utils/IntegerToBoolConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Yodii.Lab
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [ValueConversion( typeof( Int32 ), typeof( bool ) )]
+    public class IntegerToBoolConverter : IValueConverter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+        {
+            int c = (int)value;
+
+            bool result;
+            if( parameter != null )
+            {
+                // With parameter, return true if zero
+                result = c == 0 ? true : false;
+            }
+            else
+            {
+                // Without parameter, return false if zero
+                result = c == 0 ? false : true;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+        {
+            return value;
+        }
+    }
+}
