a2ff63ed680ba742b9b8313e15e6a1f499e5b80c
EmilieS <simonard@intechinfo.fr>
2014-04-02 09:43:41 +0200

    test RunningServiceWithRunningSiblingPlugin error

--- Yodii.Engine.Tests/Tests/ConfigurationSolverTests/CurrentFailure/CurrentFailureTests.cs
+++ Yodii.Engine.Tests/Tests/ConfigurationSolverTests/CurrentFailure/CurrentFailureTests.cs
@@ -58,5 +58,49 @@ namespace Yodii.Engine.Tests.ConfigurationSolverTests
                 res.CheckPluginsDisabled("P");
             });
         }
+        [Test]
+        public void RunningServiceWithRunningSiblingPlugin()
+        {
+            #region graph
+            /**
+             *                  +--------+
+             *      +---------->|   S1   |
+             *      |           |        |
+             *      |           +---+----+       
+             *      |               |            
+             *      |               |                     
+             *      |           +---+-----+              
+             *  +---+-----+     |   P     |  
+             *  |  S.1.1  |     | Running |  
+             *  | Running |     +---------+  
+             *  +----+----+   
+             *       |
+             *   +---+-----+              
+             *   |   P1    |  
+             *   |         |  
+             *   +---------+        
+             */
+            #endregion
+
+            var d = new DiscoveredInfo();
+            d.ServiceInfos.Add(new ServiceInfo("S1", d.DefaultAssembly));
+            d.ServiceInfos.Add(new ServiceInfo("S1.1", d.DefaultAssembly));
+            d.FindService("S1.1").Generalization = d.FindService("S1");
+
+            d.PluginInfos.Add(new PluginInfo("P", d.DefaultAssembly));
+            d.FindPlugin("P").Service = d.FindService("S1");
+            d.PluginInfos.Add(new PluginInfo("P1", d.DefaultAssembly));
+            d.FindPlugin("P1").Service = d.FindService("S1.1");
+
+            YodiiEngine engine = new YodiiEngine(new YodiiEngineHostMock());
+            engine.SetDiscoveredInfo(d);
+
+            IConfigurationLayer cl = engine.Configuration.Layers.Create();
+            cl.Items.Add("S1.1", ConfigurationStatus.Running);
+            cl.Items.Add("P", ConfigurationStatus.Running);
+
+            var result = engine.Start();
+            Assert.That(result.Success, Is.False);
+        }
     }
 }
