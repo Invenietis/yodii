c66850f64af8c53f7410551024b1147cb86c52d3
EmilieS <simonard@intechinfo.fr>
2014-04-08 10:29:02 +0200

    InvalidLoop test

--- Yodii.Engine.Tests/Tests/ConfigurationSolverTests/CurrentFailure/CurrentFailureTests.cs
+++ Yodii.Engine.Tests/Tests/ConfigurationSolverTests/CurrentFailure/CurrentFailureTests.cs
@@ -102,5 +102,67 @@ namespace Yodii.Engine.Tests.ConfigurationSolverTests
             var result = engine.Start();
             Assert.That(result.Success, Is.False);
         }
+
+
+        [Test]
+        public void InvalidLoop()
+        {
+            #region graph
+        /**
+             *                  +--------+                              +--------+
+             *      +---------->|Service1+-------+   *----------------->|Service2|
+             *      |           |Optional|       |   | Need Running     |Optional|   
+             *      |           +---+----+       |   |                  +---+----+
+             *      |                        +---+-----+                    |
+             *      |                        |Plugin1  |                    |
+             *      |                        |Optional |                    |
+             *  +---+------+                 +---+-----+                    |
+             *  |Service1.1|                                                |
+             *  |Optional  |-----------------+                              |
+             *  +----+-----+                 |                          +---+-----+
+             *       |                       ---------------------------|Plugin2  |
+             *       |                                  Need Running    |Optional |
+             *       |                                                  +---------+
+             *       |
+             *       |      
+             *       |      
+             *  +---+-------+
+             *  |Plugin1.1  |
+             *  |Optional   |
+             *  +-----------+
+             */
+            #endregion
+
+            var d = new DiscoveredInfo();
+            d.ServiceInfos.Add(new ServiceInfo("Service1", d.DefaultAssembly));
+            d.ServiceInfos.Add(new ServiceInfo("Service1.1", d.DefaultAssembly));
+            d.ServiceInfos.Add(new ServiceInfo("Service2", d.DefaultAssembly));
+            d.FindService("Service1.1").Generalization = d.FindService("Service1");
+
+            d.PluginInfos.Add(new PluginInfo("Plugin1", d.DefaultAssembly));
+            d.FindPlugin("Plugin1").Service = d.FindService("Service1");
+            d.PluginInfos.Add(new PluginInfo("Plugin1.1", d.DefaultAssembly));
+            d.FindPlugin("Plugin1.1").Service = d.FindService("Service1.1");
+            d.PluginInfos.Add(new PluginInfo("Plugin2", d.DefaultAssembly));
+            d.FindPlugin("Plugin2").Service = d.FindService("Service2");
+
+            d.FindPlugin("Plugin1").AddServiceReference(d.FindService("Service2"), DependencyRequirement.Running);
+            d.FindPlugin("Plugin2").AddServiceReference(d.FindService("Service1.1"), DependencyRequirement.Running);
+
+            YodiiEngine engine = new YodiiEngine(new YodiiEngineHostMock());
+            engine.SetDiscoveredInfo(d);
+
+
+
+            var result = engine.Start();
+            engine.FullStaticResolutionOnly(res =>
+            {
+                res.CheckSuccess();
+                res.CheckPluginsDisabled("Plugin1");
+                res.CheckAllPluginsRunnable("Plugin2,Plugin1.1");
+                System.Diagnostics.Debug.WriteLine(res.StaticSolvedConfiguration.FindPlugin("Plugin2").FinalConfigSolvedStatus.ToString());
+                res.CheckAllServicesRunnable("Service1,Service1.1,Service2");
+            });
+        }
     }
 }
