ae60aa9b5dd2147ae49f4f9b538d74a97a11b234
EmilieS <simonard@intechinfo.fr>
2014-06-12 19:30:59 +0200

    a couple more tests.

--- Yodii.Host.Tests/YodiiHostTests.cs
+++ Yodii.Host.Tests/YodiiHostTests.cs
@@ -25,26 +25,26 @@ namespace Yodii.Host.Tests
             IAssemblyInfo ia = discoverer.ReadAssembly( Path.GetFullPath( "Yodii.Host.Tests.dll" ) );
             IDiscoveredInfo info = discoverer.GetDiscoveredInfo();
 
-            PluginHost host = new PluginHost(); /*IYodiiEngineHost this is not enough, need access to PluginCreator & ServiceReferencesBinder*/
-            YodiiEngine engine = new YodiiEngine( host);
-            engine.SetDiscoveredInfo(info);
+            PluginHost host = new PluginHost();
+            YodiiEngine engine = new YodiiEngine( host );
+            engine.SetDiscoveredInfo( info );
 
             IConfigurationLayer cl = engine.Configuration.Layers.Create();
             cl.Items.Add( "Yodii.Host.Tests.ChoucroutePlugin", ConfigurationStatus.Optional );
-                         
+
             var result = engine.Start();
             engine.LiveInfo.FindPlugin( "Yodii.Host.Tests.ChoucroutePlugin" ).Start();
             IPluginProxy pluginProxy = host.FindLoadedPlugin( "Yodii.Host.Tests.ChoucroutePlugin", false );
-            ChoucroutePlugin choucroute = (ChoucroutePlugin) pluginProxy.RealPluginObject;
+            ChoucroutePlugin choucroute = (ChoucroutePlugin)pluginProxy.RealPluginObject;
             Assert.That( choucroute.CalledMethods.Count == 3 );
             Assert.That( host.FindLoadedPlugin( "Yodii.Host.Tests.ChoucroutePlugin", false ).Status == InternalRunningStatus.Started );
 
-            Assert.That(engine.LiveInfo.FindPlugin( "Yodii.Host.Tests.ChoucroutePlugin" ).Capability.CanStop==true);
+            Assert.That( engine.LiveInfo.FindPlugin( "Yodii.Host.Tests.ChoucroutePlugin" ).Capability.CanStop == true );
             engine.LiveInfo.FindPlugin( "Yodii.Host.Tests.ChoucroutePlugin" ).Stop();
             Assert.That( host.FindLoadedPlugin( "Yodii.Host.Tests.ChoucroutePlugin", false ).Status == InternalRunningStatus.Stopped );
-            Assert.That( choucroute.CalledMethods.Count == 5 );//plugin is stopped but we can still directly access it ?
+            Assert.That( choucroute.CalledMethods.Count == 5 );//plugin is stopped but we can still directly access it since assembly is still there.
 
-            IChoucrouteService service= (IChoucrouteService)host.ServiceHost.GetProxy(typeof( IChoucrouteService) );
+            IChoucrouteService service= (IChoucrouteService)host.ServiceHost.GetProxy( typeof( IChoucrouteService ) );
             Assert.Throws<Yodii.Model.ServiceStoppedException>( (delegate() { int i =service.CalledMethods.Count; }) );
 
             engine.LiveInfo.FindPlugin( "Yodii.Host.Tests.ChoucroutePlugin" ).Start();
@@ -59,8 +59,99 @@ namespace Yodii.Host.Tests
             Assert.That( pluginProxy == pluginProxy2 );
             Assert.That( choucroute == choucroute2 );
         }
-        //TODO (short term): see what happens if te plugin does not work properly
-        //Tester exception balancé au start
-        //Tester exception balancé au stop
+        /// <summary>
+        /// When the first setup fails, the proxy isn't even made.
+        /// </summary>
+        [Test]
+        public void IfSetupFailsChoucrouteTest2()
+        {
+
+            StandardDiscoverer discoverer = new StandardDiscoverer();
+            IAssemblyInfo ia = discoverer.ReadAssembly( Path.GetFullPath( "Yodii.Host.Tests.dll" ) );
+            IDiscoveredInfo info = discoverer.GetDiscoveredInfo();
+
+            PluginHost host = new PluginHost();
+            YodiiEngine engine = new YodiiEngine( host );
+            engine.SetDiscoveredInfo( info );
+
+            IConfigurationLayer cl = engine.Configuration.Layers.Create();
+            cl.Items.Add( "Yodii.Host.Tests.ChoucroutePlugin2", ConfigurationStatus.Optional );
+
+            var result = engine.Start();
+            engine.LiveInfo.FindPlugin( "Yodii.Host.Tests.ChoucroutePlugin2" ).Start();
+            engine.CheckStopped( "Yodii.Host.Tests.IChoucrouteService2" );
+            IPluginProxy proxy=host.FindLoadedPlugin( "Yodii.Host.Tests.ChoucroutePlugin2", false );
+            Assert.IsNull( proxy );
+        }
+        /// <summary>
+        /// The fact a stop fails is pretty much ignored.
+        /// </summary>
+        [Test]
+        public void IfStopFailsChoucrouteTest3()
+        {
+
+            StandardDiscoverer discoverer = new StandardDiscoverer();
+            IAssemblyInfo ia = discoverer.ReadAssembly( Path.GetFullPath( "Yodii.Host.Tests.dll" ) );
+            IDiscoveredInfo info = discoverer.GetDiscoveredInfo();
+
+            PluginHost host = new PluginHost(); /*IYodiiEngineHost this is not enough, need access to PluginCreator & ServiceReferencesBinder*/
+            YodiiEngine engine = new YodiiEngine( host );
+            engine.SetDiscoveredInfo( info );
+
+            IConfigurationLayer cl = engine.Configuration.Layers.Create();
+            cl.Items.Add( "Yodii.Host.Tests.ChoucroutePlugin3", ConfigurationStatus.Optional );
+
+            var result = engine.Start();
+            engine.LiveInfo.FindPlugin( "Yodii.Host.Tests.ChoucroutePlugin3" ).Start();
+            IPluginProxy pluginProxy = host.FindLoadedPlugin( "Yodii.Host.Tests.ChoucroutePlugin3", false );
+            ChoucroutePlugin3 choucroute = (ChoucroutePlugin3)pluginProxy.RealPluginObject;
+            Assert.That( choucroute.CalledMethods.Count == 3 );//cstor setup start
+            Assert.That( host.FindLoadedPlugin( "Yodii.Host.Tests.ChoucroutePlugin3", false ).Status == InternalRunningStatus.Started );
+
+            Assert.That( engine.LiveInfo.FindPlugin( "Yodii.Host.Tests.ChoucroutePlugin3" ).Capability.CanStop == true );
+            engine.LiveInfo.FindPlugin( "Yodii.Host.Tests.ChoucroutePlugin3" ).Stop();
+            Assert.That( host.FindLoadedPlugin( "Yodii.Host.Tests.ChoucroutePlugin3", false ).Status == InternalRunningStatus.Stopped );
+            Assert.That( choucroute.CalledMethods.Count == 5 );//+stop teardown
+
+            IChoucrouteService3 service= (IChoucrouteService3)host.ServiceHost.GetProxy( typeof( IChoucrouteService3 ) );
+            Assert.Throws<Yodii.Model.ServiceStoppedException>( (delegate() { int i =service.CalledMethods.Count; }) );
+
+            engine.LiveInfo.FindPlugin( "Yodii.Host.Tests.ChoucroutePlugin3" ).Start();
+            Assert.That( service.CalledMethods.Count == 7 );//+setup start
+        }
+        /// <summary>
+        /// If a start fails, the stop&teardown methods are called.
+        /// </summary>
+        [Test]
+        public void IfSartFailsChoucrouteTest4()
+        {
+
+            StandardDiscoverer discoverer = new StandardDiscoverer();
+            IAssemblyInfo ia = discoverer.ReadAssembly( Path.GetFullPath( "Yodii.Host.Tests.dll" ) );
+            IDiscoveredInfo info = discoverer.GetDiscoveredInfo();
+
+            PluginHost host = new PluginHost();
+            YodiiEngine engine = new YodiiEngine( host );
+            engine.SetDiscoveredInfo( info );
+
+            IConfigurationLayer cl = engine.Configuration.Layers.Create();
+            cl.Items.Add( "Yodii.Host.Tests.ChoucroutePlugin4", ConfigurationStatus.Optional );
+
+            var result = engine.Start();
+            engine.LiveInfo.FindPlugin( "Yodii.Host.Tests.ChoucroutePlugin4" ).Start();
+            IPluginProxy pluginProxy = host.FindLoadedPlugin( "Yodii.Host.Tests.ChoucroutePlugin4", false );
+            ChoucroutePlugin4 choucroute = (ChoucroutePlugin4)pluginProxy.RealPluginObject;
+            Assert.That( choucroute.CalledMethods.Count == 5 );//ctor, setup, start (execption), stop, teardown.
+            Assert.That( host.FindLoadedPlugin( "Yodii.Host.Tests.ChoucroutePlugin4", false ).Status == InternalRunningStatus.Stopped );
+            engine.LiveInfo.FindPlugin( "Yodii.Host.Tests.ChoucroutePlugin4" ).Stop(); //we can call stop on an allready stopped plugin.
+            Assert.That( choucroute.CalledMethods.Count == 5 ); //but this call for stop does not get to the plugin.
+
+            IChoucrouteService4 service= (IChoucrouteService4)host.ServiceHost.GetProxy( typeof( IChoucrouteService4 ) );
+            engine.CheckStopped( "Yodii.Host.Tests.IChoucrouteService4" );
+
+            engine.LiveInfo.FindPlugin( "Yodii.Host.Tests.ChoucroutePlugin4" ).Start();
+            Assert.That( choucroute.CalledMethods.Count == 9 );//+setup, start (execption), stop, teardown.
+            Assert.That( host.FindLoadedPlugin( "Yodii.Host.Tests.ChoucroutePlugin4", false ).Status == InternalRunningStatus.Stopped );
+        }
     }
 }
