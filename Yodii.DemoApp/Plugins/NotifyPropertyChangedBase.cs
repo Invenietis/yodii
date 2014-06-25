18bf471896b2ac707c2ed06164803a586ef56e69
Benjamin Crosnier <benjamin.crosnier@invenietis.com>
2014-06-25 17:25:47 +0200

    Fixed missing INotifyPropertyChanged.

--- Yodii.DemoApp/Plugins/NotifyPropertyChangedBase.cs
+++ Yodii.DemoApp/Plugins/NotifyPropertyChangedBase.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 
 namespace Yodii.DemoApp
 {
-    public abstract class NotifyPropertyChangedBase
+    public abstract class NotifyPropertyChangedBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
